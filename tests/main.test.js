89c2e60a7c1b7f021663dc514d5fe11fa18ea4fd
Javier Seixas <dev@javierseixas.com>
2021-08-06 02:01:10 -0400

    Set out fith scenario for non ordened list

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,5 +1,6 @@
 const { expect } = require("@jest/globals");
 const createTree = require("../main");
+const createTreeFromUnordered = require("../createFromUnordered");
 
 describe("createTree", () => {
   it("list with root person", () => {
@@ -165,5 +166,46 @@ describe("createTree", () => {
     };
 
     expect(createTree(input)).toEqual(expected);
+  });
+
+  it("list with three people, with reversed levels", () => {
+    const input = [
+      {
+        id: 4,
+        name: "Finance Manager 1",
+        parent: 2,
+      },
+      {
+        id: 2,
+        name: "CMO",
+        parent: 1,
+      },
+      {
+        id: 1,
+        name: "CEO",
+        parent: null,
+      },
+    ];
+    expected = {
+      id: 1,
+      name: "CEO",
+      children: [
+        {
+          id: 2,
+          name: "CMO",
+          children: [
+            {
+              id: 4,
+              name: "Finance Manager 1",
+              children: [],
+            },
+          ],
+        },
+      ],
+    };
+
+    const result = createTreeFromUnordered(input);
+
+    expect(result).toEqual(expected);
   });
 });
