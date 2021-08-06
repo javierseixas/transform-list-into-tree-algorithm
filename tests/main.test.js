13ffd56ea4bac1d7e76bdc2569e223c5e66338bb
Javier Seixas <dev@javierseixas.com>
2021-08-05 22:43:24 -0400

    Set out third scenario

--- tests/main.test.js
+++ tests/main.test.js
@@ -46,4 +46,43 @@ describe("createTree", () => {
 
     expect(createTree(input)).toEqual(expected);
   });
+
+  it("list with thre people, consecutive and ordered levels", () => {
+    const input = [
+      {
+        id: 1,
+        name: "CEO",
+        parent: null,
+      },
+      {
+        id: 2,
+        name: "CMO",
+        parent: 1,
+      },
+      {
+        id: 4,
+        name: "Finance Manager 1",
+        parent: 2,
+      },
+    ];
+    expected = {
+      id: 1,
+      name: "CEO",
+      children: [
+        {
+          id: 2,
+          name: "CMO",
+          children: [
+            {
+              id: 4,
+              name: "Finance Manager 1",
+              children: [],
+            },
+          ],
+        },
+      ],
+    };
+
+    expect(createTree(input)).toEqual(expected);
+  });
 });
