153c6ebecef699923bcf8c0f6a7bc40be6e4636d
Javier Seixas <dev@javierseixas.com>
2021-08-06 00:31:28 -0400

    Add already passing test for interview wording

--- tests/main.test.js
+++ tests/main.test.js
@@ -85,4 +85,85 @@ describe("createTree", () => {
 
     expect(createTree(input)).toEqual(expected);
   });
+
+  it("list according to test wording in interview", () => {
+    const input = [
+      {
+        id: 1,
+        name: "CEO",
+        parent: null,
+      },
+      {
+        id: 2,
+        name: "CMO",
+        parent: 1,
+      },
+      {
+        id: 3,
+        name: "CTO",
+        parent: 1,
+      },
+      {
+        id: 4,
+        name: "Finance Manager 1",
+        parent: 2,
+      },
+      {
+        id: 5,
+        name: "Finance Manager 2",
+        parent: 2,
+      },
+      {
+        id: 6,
+        name: "Tech Manager 1",
+        parent: 3,
+      },
+      {
+        id: 7,
+        name: "Tech Manager 2",
+        parent: 3,
+      },
+    ];
+
+    expected = {
+      id: 1,
+      name: "CEO",
+      children: [
+        {
+          id: 2,
+          name: "CMO",
+          children: [
+            {
+              id: 4,
+              name: "Finance Manager 1",
+              children: [],
+            },
+            {
+              id: 5,
+              name: "Finance Manager 2",
+              children: [],
+            },
+          ],
+        },
+        {
+          id: 3,
+          name: "CTO",
+          children: [
+            {
+              id: 6,
+              name: "Tech Manager 1",
+              children: [],
+            },
+            {
+              id: 7,
+              name: "Tech Manager 2",
+              children: [],
+            },
+          ],
+        },
+      ],
+    };
+
+    expect(createTree(input)).toEqual(expected);
+  });
 });
