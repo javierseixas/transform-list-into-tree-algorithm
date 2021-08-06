7401f5c057eb9eb008d98b4f5662fa5a5d2b98a5
Javier Seixas <dev@javierseixas.com>
2021-08-06 14:51:24 -0400

    Change test for avoiding conflicts

--- tests/createTreeFromUnordered.test.js
+++ tests/createTreeFromUnordered.test.js
@@ -5,6 +5,47 @@ describe("createTree", () => {
   it("list with three people, with reversed levels", () => {
     const input = [
       {
+        id: 40,
+        name: "Finance Manager 1",
+        parent: 20,
+      },
+      {
+        id: 20,
+        name: "CMO",
+        parent: 10,
+      },
+      {
+        id: 10,
+        name: "CEO",
+        parent: null,
+      },
+    ];
+    const expected = {
+      id: 10,
+      name: "CEO",
+      children: [
+        {
+          id: 20,
+          name: "CMO",
+          children: [
+            {
+              id: 40,
+              name: "Finance Manager 1",
+              children: [],
+            },
+          ],
+        },
+      ],
+    };
+
+    const result = createTreeFromUnordered(input);
+
+    expect(result).toEqual(expected);
+  });
+
+  it("list with three people, with reversed levelssssss", () => {
+    const input = [
+      {
         id: 4,
         name: "Finance Manager 1",
         parent: 2,
@@ -19,8 +60,13 @@ describe("createTree", () => {
         name: "CEO",
         parent: null,
       },
+      {
+        id: 76,
+        name: "Finance Manager 2",
+        parent: 2,
+      },
     ];
-    expected = {
+    const expected = {
       id: 1,
       name: "CEO",
       children: [
@@ -31,6 +77,11 @@ describe("createTree", () => {
             {
               id: 4,
               name: "Finance Manager 1",
+              children: [],
+            },
+            {
+              id: 76,
+              name: "Finance Manager 2",
               children: [],
             },
           ],
