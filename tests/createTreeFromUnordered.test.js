1ce6ef569fc665ffaf9be2a79a959f1d8dc72493
Javier Seixas <dev@javierseixas.com>
2021-08-07 10:49:37 -0400

    Add test for covering unordered list and ids

--- tests/createTreeFromUnordered.test.js
+++ tests/createTreeFromUnordered.test.js
@@ -86,8 +86,56 @@ describe("createTree", () => {
       ],
     };
 
-    const result = createTreeFromUnordered(input);
+    expect(createTreeFromUnordered(input)).toEqual(expected);
+  });
 
-    expect(result).toEqual(expected);
+  // CEO doesn't have first id
+  it("list with four people, unordered ids", () => {
+    const input = [
+      {
+        id: 8,
+        name: "Finance Manager 1",
+        parent: 20,
+      },
+      {
+        id: 20,
+        name: "CMO",
+        parent: 15,
+      },
+      {
+        id: 15,
+        name: "CEO",
+        parent: null,
+      },
+      {
+        id: 16,
+        name: "Finance Manager 2",
+        parent: 20,
+      },
+    ];
+    const expected = {
+      id: 15,
+      name: "CEO",
+      children: [
+        {
+          id: 20,
+          name: "CMO",
+          children: [
+            {
+              id: 8,
+              name: "Finance Manager 1",
+              children: [],
+            },
+            {
+              id: 16,
+              name: "Finance Manager 2",
+              children: [],
+            },
+          ],
+        },
+      ],
+    };
+
+    expect(createTreeFromUnordered(input)).toEqual(expected);
   });
 });
